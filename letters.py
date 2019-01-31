4c8eabe258b6d82d96576e0bc7823aec0811fad2
Jordan Winfery <jwinf843@gmail.com>
2019-01-31 07:26:03 +0000

    Initialized the project and began laying a foundation for later use

---
+++ letters.py
@@ -0,0 +1,75 @@
+letters = {
+    'A': '  AAA   \n AAAAA  \nAA   AA \nAAAAAAA \nAA   AA \n        ',
+    'B': 'BBBBB   \nBB   B  \nBBBBBB  \nBB   BB \nBBBBBB  \n        ',
+    'C': ' CCCCC  \nCC    C \nCC      \nCC    C \n CCCCC  \n        ', 
+    'D': 'DDDDD   \nDD  DD  \nDD   DD \nDD   DD \nDDDDDD  \n        ', 
+    'E': 'EEEEEEE \nEE      \nEEEEE   \nEE      \nEEEEEEE \n        ',
+    'F': 'FFFFFFF \nFF      \nFFFF    \nFF      \nFF      \n        ',
+    'G': '  GGGG  \n GG  GG \nGG      \nGG   GG \n GGGGGG \n        ', 
+    'H': 'HH   HH \nHH   HH \nHHHHHHH \nHH   HH \nHH   HH \n        ', 
+    'I': 'IIIII \n III  \n III  \n III  \nIIIII \n      ',
+    'J': '    JJJ \n    JJJ \n    JJJ \nJJ  JJJ \n JJJJJ  \n        ', 
+    'K': 'KK  KK \nKK KK  \nKKKK   \nKK KK  \nKK  KK \n       ',
+    'L': 'LL      \nLL      \nLL      \nLL      \nLLLLLLL \n        ', 
+    'M': 'MM    MM \nMMM  MMM \nMM MM MM \nMM    MM \nMM    MM \n         ',
+    'N': 'NN   NN \nNNN  NN \nNN N NN \nNN  NNN \nNN   NN \n        ,
+    'O': ''
+    'P': ''
+    'Q': ''
+    'R': ''
+    'S': ''
+    'T': ''
+    'U': ''
+    'V': ''
+    'W': ''
+    'X': ''
+    'Y': ''
+    'Z': ''
+    'a': ''
+    'b': ''
+    'c': ''
+    'd': ''
+    'e': ''
+    'f': ''
+    'g': ''
+    'h': ''
+    'i': ''
+    'j': ''
+    'k': ''
+    'l': ''
+    'm': ''
+    'n': ''
+    'o': ''
+    'p': ''
+    'q': ''
+    'r': ''
+    's': ''
+    't': ''
+    'u': ''
+    'v': ''
+    'w': ''
+    'x': ''
+    'y': ''
+    'z': ''
+    ' ': ''
+    ',': ''
+    '.': ''
+    '!': ''
+    '?': ''
+    '"': ''
+    "'": ''
+    '-': ''
+    '_': ''
+    '=': ''
+    '<': ''
+    '>': ''
+    '@': ''
+    '#': ''
+    '$': ''
+    '%': ''
+    '^': ''
+    '&': ''
+    '*': ''
+    '(': ''
+    ')': ''
+}
---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+def super_print(string):
+    
+    for letter in string:
+        a = '''  AAA         
+ AAAAA        
+AA   AA       
+AAAAAAA       
+AA   AA       '''
+    print(a)
+
+super_print("Hello")
